4f45325308eb9c1c557f99c83e659e185863b972
marey111@126.com <mareymarey111>
2020-09-19 19:06:43 +1000

    modify main file

--- Codes/main.c
+++ Codes/main.c
@@ -2,10 +2,20 @@
 // Date: 2020-09-10
 // C Program: Hello world
 
+// 2521，9024 学完，debug能力，还有编程思维
+
 #include <stdio.h>
 
-int main(void) {
-
-    printf("Hello World\n");
+int main(int argc, char *args[]) {
+    int number = 0;
+    while (1) {
+        printf("请输入一个数字：");
+        if (scanf("%d", &number) == 1) {
+            printf("your input is %d\n", number);
+        }
+        if (number > 100) {
+            break;
+        }
+    }
     return 0;
 }
