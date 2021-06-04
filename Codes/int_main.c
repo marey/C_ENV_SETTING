7c8ba9c3ce9c1a7e239843cd7a6aae703818865e
marey111@126.com <mareymarey111>
2021-06-04 10:36:34 +1000

    add main02.c

---
+++ Codes/int_main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void test01() {
+    int n;
+    printf("n is : %d\n", n);
+}
+
+// 这是我的第一个函数
+int main(int argc, char *argv[]) {
+
+    test01();
+
+    return 0;
+}
