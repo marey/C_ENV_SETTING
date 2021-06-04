f35fe5ec70362956231b8a361a388546c7fefdc3
marey111@126.com <mareymarey111>
2021-06-04 10:51:12 +1000

    reinit

--- Codes/main.c
+++ Codes/main.c
@@ -2,37 +2,12 @@
 // Date: 2020-09-10
 // C Program: Hello world
 
-// 2521，9024 学完，debug能力，还有编程思维
-// OK fine
 #include <stdio.h>
 #include "list.h"
 
 int main(int argc, char *argv[]) {
-    List list1 = createList();
-    // 创建了第一户
-    insertListToLast(list1, 1, 3);
-    insertListToLast(list1, 2, 5);
-    insertListToLast(list1, 3, 100);
 
-    // 打印所有的Node
-    printList(list1);
+    printf("this is my first program\n");
 
-    List list2 = createList();
-    // 创建了第一户
-    insertListToLast(list2, 1, 3);
-    insertListToLast(list2, 2, 5);
-    insertListToLast(list2, 4, 100);
-    printList(list2);
-
-    List list3 = intersection(list1, list2);
-    List list4 = unionList(list1, list2);
-
-    printList(list3);
-    printList(list4);
-
-
-    freeList(list1);
-    freeList(list2);
-    freeList(list3);
-    freeList(list4);
+    return 0;
 }
--- Codes/main02.c
+++ Codes/main02.c
@@ -4,75 +4,13 @@
 // 这是我的第一个函数
 int main(int argc, char *argv[]) {
 
-    // printf("number of argument: %d\n", argc);
-//    int total = 0;
-//    for(int i = 1; i< argc; i++) {
-//        printf("param index: %d, value: %s\n", i, argv[i]);
-//        total = total + atoi(argv[i]);
-//    }
-//    printf("total: %d\n", total);
-
-
-
-
-
-    int n = 10;
-
-    // 没有初始化任何内容
-    int eli_numbers[n];
-    // 不指定数组的长度
-    int numbers01[] = {1,2,3,4,5};
-    // 指定数组的长度
-    int numbers02[5] = {1,2,3,4,5};
-    // 仅初始化期中的某些值
-    int numbers03[5] = {1,2,3};
-    // 根据变量
-    int m = 10;
-    int* eli_nums = malloc(sizeof(int) * m);
-
-
-    eli_numbers[0] = 100;
-    eli_numbers[1] = 35;
-
-    eli_numbers[8] = 77;
-    // 地址
-    printf("the address of array: %p\n", eli_numbers);
-    // 查看地址上面的value
-    printf("the value of address: %d\n", *eli_numbers);
-    // 数组
-    printf("the item of array: %d\n", eli_numbers[0]);
-    printf("the item of array: %p\n", &eli_numbers[0]);
-
-    printf("the address of array: %p\n", eli_numbers + 8);
-    // 查看地址上面的value
-    printf("the value of address: %d\n", *(eli_numbers + 8));
-    // 数组
-    printf("the item of array: %d\n", eli_numbers[8]);
-    printf("the item of array: %p\n", &eli_numbers[8]);
-
-
-    int m = 10;
-
-    int* eli_nums = malloc(sizeof(int) * m);
-    eli_nums[0] = 100;
-    eli_nums[1] = 35;
-    eli_nums[8] = 77;
-    printf("====================\n");
-    // 地址
-    printf("the address of array: %p\n", eli_nums);
-    // 查看地址上面的value
-    printf("the value of address: %d\n", *eli_nums);
-    // 数组
-    printf("the item of array: %d\n", eli_nums[0]);
-    printf("the item of array: %p\n", &eli_nums[0]);
-    printf("the address of array: %p\n", eli_nums + 8);
-    // 查看地址上面的value
-    printf("the value of address: %d\n", *(eli_nums + 8));
-    // 数组
-    printf("the item of array: %d\n", eli_nums[8]);
-    printf("the item of array: %p\n", &eli_nums[8]);
-
-    free(eli_nums);
+    printf("number of argument: %d\n", argc);
+    int total = 0;
+    for (int i = 1; i < argc; i++) {
+        printf("param index: %d, value: %s\n", i, argv[i]);
+        total = total + atoi(argv[i]);
+    }
+    printf("total: %d\n", total);
 
     return 0;
 }
